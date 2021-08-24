8bb86b2e2aa83e84c7c18f6f3d987323a95eca31
Degan90 <poorya.degan@gmail.com>
2021-08-24 10:41:42 -0400

    quick fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -51,6 +51,19 @@ function Home(props) {
               </p>
             </div>
           </div>
+          <div className="tool">
+            <div className="toolImage">
+              <img className="img" src={tool} />
+            </div>
+
+            <div className="des">
+              <h6>pdf converter</h6>
+              <p>
+                This tool converts the format of your images Key Features <br />
+                #pdf #converter
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
